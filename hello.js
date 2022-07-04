f810cd99eb9696134a164f93832cabdd05a563c5
KenjiRN <kenji_nakajima@hotmail.com>
2022-07-04 15:01:15 -0400

    sup

---
+++ hello.js
@@ -0,0 +1,10 @@
+feather.replace()    <!--ACRESCENTA ESTE SCRIPT PARA TER OS ICONES-->
+            document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+                anchor.addEventListener('click', function (e) {
+                    e.preventDefault();
+
+                    document.querySelector(this.getAttribute('href')).scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                });
+            });
